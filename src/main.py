6d447db62329ec0ec8449100adf1f2d6d74f6d01
KyrylOTheRylo <kiril.mosiychuk@gmail.com>
2022-11-23 19:38:07 +0200

    using main as client for now

--- src/main.py
+++ src/main.py
@@ -1,14 +1,14 @@
-import requests
+from requestapi import get_exact_weather, getweatherbycity
 
-url = "https://weatherapi-com.p.rapidapi.com/forecast.json"
-
-querystring = {"q": "G2J", "days": "3"}
-
-headers = {
+HEADERS = {
     "X-RapidAPI-Key": "0187324a52msh0eaa900ff5d671dp18c951jsn4147fe93861d",
     "X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com"
 }
+url = "https://weatherapi-com.p.rapidapi.com/forecast.json"
 
-response = requests.request("GET", url, headers=headers, params=querystring)
+if __name__ == "__main__":
+    response = getweatherbycity(city="Paris", days=6, url=url, headers=HEADERS)
+    info = response.json()
 
-print(response.text)
+    for x in get_exact_weather(info):
+        print(x)
